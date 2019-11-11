df1067be18508d5dac59c6e9db8df6b5b1039e6d
Robin Jiménez Newman <robinjimeneznewman@gmail.com>
2019-11-11 09:18:27 +0100

    Added script to detect daytime and sun position

---
+++ js/girasol.js
@@ -0,0 +1,32 @@
+var output = document.querySelector('.output');
+var music = document.querySelector('audio');
+
+function setBackground(position) {
+    var now = new Date();
+
+    console.log(now);
+    var times = SunCalc.getTimes(new Date(), position.coords.latitude, position.coords.longitude);
+    console.log(times);
+
+
+    if (now < times.dawn || now > times.dusk) {
+        $('body').addClass('night');
+    } else if (now >= times.dawn && now < times.sunriseEnd) {
+        $('body').addClass('sunrise');
+    } else if (now >= times.sunriseEnd && now < times.solarNoon) {
+        $('body').addClass('morning');
+    } else if (now >= times.solarNoon && now < times.sunsetStart) {
+        $('body').addClass('afternoon');
+    } else if (now >= times.sunsetStart && now < times.dusk) {
+        $('body').addClass('sunset');
+    }
+
+}
+
+$(document).ready(function () {
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(setBackground);
+    }
+
+});
