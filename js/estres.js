b5b6853afd043ef17b3d202f9f82d50d1a9132ff
Robin Jiménez Newman <robinjimeneznewman@gmail.com>
2019-12-02 18:41:59 +0100

    Estrés major update: creates spheres on touch, with AmmoJS physics, fixed rotation and turbulence added

--- js/estres.js
+++ js/estres.js
@@ -1,6 +1,6 @@
 /*
     ---------------------
-    MIRACLE - Main Module
+    ESTRES - Main Module
     ---------------------
  */
 
@@ -24,7 +24,7 @@ document.getElementById('start-button').onclick = requestPermissions;
 
 // For devices that need permission requesting
 function requestPermissions() {
-    if (isMobile && typeof(DeviceMotionEvent) !== 'undefined' && typeof(DeviceMotionEvent.requestPermission) === 'function') {
+    if (isMobile && typeof (DeviceMotionEvent) !== 'undefined' && typeof (DeviceMotionEvent.requestPermission) === 'function') {
         DeviceMotionEvent.requestPermission()
             .then(response => {
                 if (response == 'granted') {
@@ -39,6 +39,7 @@ function requestPermissions() {
     createScene();
 }
 
+
 // SCENE CREATION
 
 function createScene() {
@@ -47,13 +48,20 @@ function createScene() {
     var width = window.innerWidth;
     var height = window.innerHeight;
 
-    var scene, renderer, camera, composer, filmPass;
+    var raycaster = new THREE.Raycaster();
+
+    var physicsWorld;
+    var rigidBodies = [], tmpTrans;
+    var scene, renderer, camera, composer, filmPass, generatorPlane;
     var tunnels = [];
     var then = 0;
 
-    init();
+    Ammo().then(init);
 
     function init() {
+        tmpTrans = new Ammo.btTransform();
+
+        setupPhysicsWorld();
 
         sceneSetup();
         sceneElements();
@@ -62,6 +70,9 @@ function createScene() {
 
         if (!isMobile()) {
             window.addEventListener("mousemove", onInputMove);
+            window.addEventListener("click", handleClick);
+        } else {
+            window.addEventListener("touchstart", handleClick);
         }
 
         resize();
@@ -76,16 +87,106 @@ function createScene() {
 
     }
 
+    // PHYSICS SETUP
+
+    function setupPhysicsWorld() {
+
+        let collisionConfiguration = new Ammo.btDefaultCollisionConfiguration(),
+            dispatcher = new Ammo.btCollisionDispatcher(collisionConfiguration),
+            overlappingPairCache = new Ammo.btDbvtBroadphase(),
+            solver = new Ammo.btSequentialImpulseConstraintSolver();
+
+        physicsWorld = new Ammo.btDiscreteDynamicsWorld(dispatcher, overlappingPairCache, solver, collisionConfiguration);
+        physicsWorld.setGravity(new Ammo.btVector3(0, 0, -10));
+
+    }
+
+    function createBall(x, y) {
+
+        let pos = {x: x, y: y, z: -20};
+        let radius = Math.random() + 1;
+        let quat = {x: 0, y: 0, z: 0, w: 1};
+        let mass = Math.random() * 20;
+
+        //threeJS Section
+        let ball = new THREE.Mesh(new THREE.SphereBufferGeometry(radius, radius * 8, radius * 8), new THREE.MeshPhongMaterial({color: 0xaabbcc}));
+
+        ball.position.set(pos.x, pos.y, pos.z);
+
+        ball.castShadow = true;
+        ball.receiveShadow = true;
+
+        scene.add(ball);
+
+        //Ammojs Section
+        let transform = new Ammo.btTransform();
+        transform.setIdentity();
+        transform.setOrigin(new Ammo.btVector3(pos.x, pos.y, pos.z));
+        transform.setRotation(new Ammo.btQuaternion(quat.x, quat.y, quat.z, quat.w));
+        let motionState = new Ammo.btDefaultMotionState(transform);
+
+        let colShape = new Ammo.btSphereShape(radius);
+        colShape.setMargin(0.05);
+
+        let localInertia = new Ammo.btVector3(0, 0, 0);
+        colShape.calculateLocalInertia(mass, localInertia);
+
+        let rbInfo = new Ammo.btRigidBodyConstructionInfo(mass, motionState, colShape, localInertia);
+        let body = new Ammo.btRigidBody(rbInfo);
+
+
+        physicsWorld.addRigidBody(body);
+
+        ball.userData.physicsBody = body;
+        ball.userData.turbulence = Math.random();
+
+        rigidBodies.push(ball);
+    }
+
+
+    function updatePhysics(deltaTime) {
+
+        // Step world
+        physicsWorld.stepSimulation(deltaTime, 10);
+
+        // Update rigid bodies
+        for (let i = 0; i < rigidBodies.length; i++) {
+            let objThree = rigidBodies[i];
+            let objAmmo = objThree.userData.physicsBody;
+            let ms = objAmmo.getMotionState();
+            if (ms) {
+
+
+                ms.getWorldTransform(tmpTrans);
+                let p = tmpTrans.getOrigin();
+                let q = tmpTrans.getRotation();
+
+                let turbulenceX = Math.sin(performance.now() * 0.005 * objThree.userData.turbulence);
+                let turbulenceY = Math.cos(performance.now() * 0.005 * objThree.userData.turbulence);
+
+                objThree.position.set(p.x() + turbulenceX, p.y() + turbulenceY, p.z());
+                objThree.quaternion.set(q.x(), q.y(), q.z(), q.w());
+
+                if (objThree.position.z < -500) {
+                    physicsWorld.removeCollisionObject(objAmmo);
+                    scene.remove(objThree);
+                }
+
+            }
+        }
+
+    }
+
     function animationSetup() {
         let start;
 
         tl = anime.timeline({
             easing: 'easeInOutSine',
-            begin: function(anim) {
+            begin: function (anim) {
                 start = new Date().getTime();
             },
             update: function (anim) {
-                output.innerHTML = new Date().getTime() - start;
+                //output.innerHTML = new Date().getTime() - start;
             }
         });
 
@@ -143,12 +244,17 @@ function createScene() {
     }
 
     function sceneElements() {
+        var geometry = new THREE.PlaneBufferGeometry(100, 100, 100, 100);
+        var material = new THREE.MeshBasicMaterial({visible: false});
+        generatorPlane = new THREE.Mesh(geometry, material);
+        generatorPlane.position.z = -20;
+        scene.add(generatorPlane);
 
-        var geometry = new THREE.CylinderGeometry(200, 50, 1000, 32, 32,  true);
-        geometry.rotateX(Math.PI/2);
-        var sphere = new THREE.SphereBufferGeometry(2, 8,8);
+        geometry = new THREE.CylinderGeometry(200, 50, 1000, 32, 32, true);
+        geometry.rotateX(Math.PI / 2);
+        var sphere = new THREE.SphereBufferGeometry(2, 8, 8);
 
-        var material = new THREE.MeshBasicMaterial({
+        material = new THREE.MeshBasicMaterial({
             color: 0xf4f4f4,
             transparent: true,
             side: THREE.BackSide,
@@ -164,7 +270,7 @@ function createScene() {
         for (let i = 0; i < 1; i++) {
             tunnels.push(new THREE.Mesh(geometry, material));
             tunnels[i].position.z = -600;
-            tunnels[i].scale.copy(new THREE.Vector3(1.5,1.5,1));
+            tunnels[i].scale.copy(new THREE.Vector3(1.5, 1.5, 1));
             scene.add(tunnels[i]);
         }
 
@@ -172,10 +278,10 @@ function createScene() {
 
     function sceneTextures() {
         // pallete
-        new THREE.TextureLoader().load('img/waves.png', function (texture) {
+        /*new THREE.TextureLoader().load('img/waves.png', function (texture) {
             //terrain.material.uniforms.palette.value = texture;
             //terrain.material.needsUpdate = true;
-        });
+        });*/
     }
 
     function resize() {
@@ -185,6 +291,25 @@ function createScene() {
         camera.updateProjectionMatrix();
 
         renderer.setSize(width, height);
+    }
+
+    function handleClick(e) {
+        e.preventDefault();
+
+        var screenPos = new THREE.Vector2();
+        if (e.type === "click") {
+            screenPos.x = ( e.clientX / width ) * 2 - 1;
+            screenPos.y = - ( e.clientY / height ) * 2 + 1;
+        } else {
+            screenPos.x = (e.changedTouches[0].pageX / width ) * 2 - 1;
+            screenPos.y = - (e.changedTouches[0].pageY  / height ) * 2 + 1;
+        }
+
+        raycaster.setFromCamera( screenPos, camera );
+        let intersection = raycaster.intersectObject( generatorPlane )[0];
+
+        createBall(intersection.point.x, intersection.point.y);
+
     }
 
     function onInputMove(e) {
@@ -199,17 +324,17 @@ function createScene() {
 
     }
 
-    onDeviceMove = function(e) {
+    onDeviceMove = function (e) {
         e.preventDefault();
 
         let alpha = e.alpha;
-        let gamma = e.gamma;
+        //let gamma = e.gamma;
         let beta = e.beta;
 
-        // Normalizing the alpha range from -90 to 90.
-        if (alpha >= 0 && alpha <= 90) {
+        // Normalizing the alpha range from -180 to 180.
+        if (alpha >= 0 && alpha <= 180) {
             alpha *= -1;
-        } else if (alpha => 270 && alpha <= 360) {
+        } else if (alpha => 180 && alpha <= 360) {
             alpha = 360 - alpha;
         }
 
@@ -217,11 +342,11 @@ function createScene() {
         if (beta > 90) alpha *= -1;
 
         // Limiting beta range
-        if (beta > 135) beta = 135;
-        if (beta < 0) beta = 0;
+        //if (beta > 135) beta = 135;
+        //if (beta < 0) beta = 0;
 
         input.a = alpha;
-        input.g = gamma;
+        //input.g = gamma;
         input.b = beta;
 
     };
@@ -234,25 +359,28 @@ function createScene() {
         then = time;
 
         if (isMobile()) {
-            input.aPrev = lerp(input.aPrev, input.a, 0.1 * map(input.bPrev, 0, 90, 1, 0.5));
-            input.bPrev = lerp(input.bPrev, input.b, 0.1);
-            /*terrain.material.uniforms.distortCenter.value = map(input.aPrev, -90, 90, -0.02, 0.02);
-            terrain.material.uniforms.waveHeight.value = map(input.bPrev, 0, 135, 0.01, 0.5);
-            terrain.material.uniforms.waveSize.value = map(input.bPrev, 0, 135, 15.0, 20.0);*/
 
-            camera.rotation.z = Math.sin(map(input.aPrev, -90, 90, 90 * Math.PI / 180, -90 * Math.PI / 180));
+            // Fixes discontinuity at -180/180
+            if (Math.abs(input.a - input.aPrev) > 270) {
+                input.aPrev = 2*input.a + input.aPrev;
+            } else {
+                input.aPrev = lerp(input.aPrev, input.a, 0.1);
+            }
+
+            input.bPrev = lerp(input.bPrev, input.b, 0.1);
+
+            camera.rotation.z = map(input.aPrev, -180, 180, Math.PI, -Math.PI);
 
 
         } else {
             input.xDamped = lerp(input.xDamped, input.x, 0.1);
             input.yDamped = lerp(input.yDamped, input.y, 0.01);
-            /*terrain.material.uniforms.distortCenter.value = map(input.xDamped, 0, width, -0.02, 0.02);
-            terrain.material.uniforms.waveHeight.value = map(input.yDamped, 0, height, 0.01, 0.5);
-            terrain.material.uniforms.waveSize.value = map(input.yDamped, 0, height, 15.0, 20.0);*/
 
             camera.rotation.z = 0.25 * Math.sin(map(input.xDamped, 0, width, 90 * Math.PI / 180, -90 * Math.PI / 180));
 
         }
+
+        updatePhysics(deltaTime);
 
         composer.render(deltaTime);
         requestAnimationFrame(render);
